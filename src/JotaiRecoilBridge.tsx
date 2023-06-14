464fd6339bfe4242104c9ab8db1e33a46d4de9ee
Steve Molitor <steve.molitor@zapier.com>
2023-06-14 14:38:29 -0500

    RecoilStore attempt
    hangs

--- src/JotaiRecoilBridge.tsx
+++ src/JotaiRecoilBridge.tsx
@@ -4,33 +4,78 @@ import {
   Provider,
   useAtomValue as useJotaiAtomValue,
 } from "jotai";
-import { useCallback } from "react";
+import { ReactNode, useCallback, useEffect } from "react";
 import {
+  atom as recoilAtom,
   selector as recoilSelector,
   RecoilRoot,
   useRecoilState,
-  selectorFamily as recoilSelectorFamily,
+  useRecoilCallback,
+  RecoilValue,
+  RecoilState,
 } from "recoil";
+
+type RecoilGet = <T>(state: RecoilValue<T>) => T;
+
+type RecoilUpdater<T> = T | ((val: T) => T);
+
+type RecoilSet = <T>(
+  state: RecoilState<T>,
+  valOrUpdater: RecoilUpdater<T>
+) => void;
+
+interface RecoilStore {
+  get: RecoilGet;
+  set: RecoilSet;
+}
+
+let recoilStore: RecoilStore = {
+  get: () => {
+    throw new Error("recoilStore not initialized");
+  },
+  set: () => {
+    throw new Error("recoilStore not initialized");
+  },
+};
+
+const RecoilStoreProvider = () => {
+  const get = useRecoilCallback(
+    ({ snapshot }) =>
+      <T,>(state: RecoilValue<T>) =>
+        snapshot.getLoadable(state).contents,
+    []
+  );
+
+  const set = useRecoilCallback(
+    ({ set }) =>
+      <T,>(state: RecoilState<T>, valOrUpdater: RecoilUpdater<T>) => {
+        set(state, valOrUpdater);
+      }
+  );
+
+  recoilStore = { get, set };
+
+  return null;
+};
 
 const jotaiStore = createStore();
 
 const jotaiCount = jotaiAtom(0);
 
-let familyParam = 0;
-
-const recoilCountFamily = recoilSelectorFamily<number, number>({
-  key: "recoilFamily",
-  get: (_param) => () => jotaiStore.get(jotaiCount),
-  set: (_param) => (_, newCount) => {
-    jotaiStore.set(jotaiCount, newCount as number);
-  },
+const alwaysChanging = recoilAtom({
+  key: "alwaysChanging",
+  default: 0,
 });
 
 const recoilCount = recoilSelector<number>({
   key: "recoilProxy",
-  get: ({ get }) => get(recoilCountFamily(familyParam++)),
-  set: ({ set }, newCount) => {
-    set(recoilCountFamily(familyParam), newCount);
+  get: ({ get }) => {
+    const n = get(alwaysChanging);
+    recoilStore.set(alwaysChanging, n + 1);
+    return jotaiStore.get(jotaiCount);
+  },
+  set: (_, newCount) => {
+    jotaiStore.set(jotaiCount, newCount as number);
   },
 });
 
@@ -55,6 +100,7 @@ export const JotaiRecoilBridge = () => {
   return (
     <Provider store={jotaiStore}>
       <RecoilRoot>
+        <RecoilStoreProvider />
         <Count />
       </RecoilRoot>
     </Provider>
