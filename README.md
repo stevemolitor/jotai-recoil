897c209be69f581f9d59edc676dc3e22c557f516
Steve Molitor <steve.molitor@zapier.com>
2023-06-14 14:38:29 -0500

    RecoilStore attempt
    hangs

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
-# Jotai / Recoil Tic Tac Toe
+# Jotai / Recoil Sync 
 
-Implement a tic tac toe game in Jotai and Recoil. It's deliberately over-engineered a little to experiment with Jotai and Recoil features, and to avoid re-rendering squares more than necessary. That's overkill here, but gives us a feel for how things would work in a larger application.
+Sync from recoil to Jotai via global RecoilStore object that can be accessed via Jotai. The RecoilStore implementation is similar to [recoil-nexus](https://www.npmjs.com/package/recoil-nexus).
+
+To make it work, the jotai to recoil proxy atom needs to force a re-evaluation by incrementting a dummy `forceEval` jotai atom every time it is set, and the jotai proxy atom getter needs to evaluate that dummy `forceEval` atom.
+
+It works going from jotai to recoil, but not vice-versa. We'd have to play similar tricks on the recoil side to make it truly a two way sync.
+
+Overally it feels hacky and brittle.
+
+
--- src/JotaiRecoilBridge.tsx
+++ src/JotaiRecoilBridge.tsx
@@ -1,61 +1,110 @@
+import { atom as jotaiAtom, Provider, useAtom as useJotaiAtom } from "jotai";
+import { ReactNode, useCallback } from "react";
 import {
-  atom as jotaiAtom,
-  createStore,
-  Provider,
-  useAtomValue as useJotaiAtomValue,
-} from "jotai";
-import { useCallback } from "react";
-import {
-  selector as recoilSelector,
+  atom as recoilAtom,
   RecoilRoot,
   useRecoilState,
-  selectorFamily as recoilSelectorFamily,
+  useRecoilCallback,
+  RecoilValue,
+  RecoilState,
 } from "recoil";
 
-const jotaiStore = createStore();
+type RecoilGet = <T>(state: RecoilValue<T>) => T;
 
-const jotaiCount = jotaiAtom(0);
+type RecoilUpdater<T> = T | ((val: T) => T);
 
-let familyParam = 0;
+type RecoilSet = <T>(
+  state: RecoilState<T>,
+  valOrUpdater: RecoilUpdater<T>
+) => void;
 
-const recoilCountFamily = recoilSelectorFamily<number, number>({
-  key: "recoilFamily",
-  get: (_param) => () => jotaiStore.get(jotaiCount),
-  set: (_param) => (_, newCount) => {
-    jotaiStore.set(jotaiCount, newCount as number);
+interface RecoilStore {
+  get: RecoilGet;
+  set: RecoilSet;
+}
+
+let recoilStore: RecoilStore = {
+  get: () => {
+    throw new Error("recoilStore not initialized");
   },
+  set: () => {
+    throw new Error("recoilStore not initialized");
+  },
+};
+
+const RecoilStoreProvider = ({ children }: { children: ReactNode }) => {
+  const get = useRecoilCallback(
+    ({ snapshot }) =>
+      <T,>(state: RecoilValue<T>) =>
+        snapshot.getLoadable(state).contents,
+    []
+  );
+
+  const set = useRecoilCallback(
+    ({ set }) =>
+      <T,>(state: RecoilState<T>, valOrUpdater: RecoilUpdater<T>) => {
+        set(state, valOrUpdater);
+      }
+  );
+
+  recoilStore = { get, set };
+
+  return <>{children}</>;
+};
+
+const recoilCount = recoilAtom<number>({
+  key: "recoilCount",
+  default: 0,
 });
 
-const recoilCount = recoilSelector<number>({
-  key: "recoilProxy",
-  get: ({ get }) => get(recoilCountFamily(familyParam++)),
-  set: ({ set }, newCount) => {
-    set(recoilCountFamily(familyParam), newCount);
+const forceJotaiEval = jotaiAtom(0);
+
+const jotaiCount = jotaiAtom(
+  (get) => {
+    console.log("get jotai");
+    get(forceJotaiEval);
+    return recoilStore.get(recoilCount);
   },
-});
+  (_get, set, newCount: number) => {
+    recoilStore.set(recoilCount, newCount);
+    set(forceJotaiEval, (val) => val + 1);
+  }
+);
 
 const Count = () => {
   const [rCount, setRCount] = useRecoilState(recoilCount);
-  const jCount = useJotaiAtomValue(jotaiCount);
+  const [jCount, setJCount] = useJotaiAtom(jotaiCount);
 
-  const increment = useCallback(() => {
+  const incrementRecoil = useCallback(() => {
     setRCount((count) => count + 1);
   }, [rCount, setRCount]);
 
+  const incrementJotai = useCallback(() => {
+    console.log("incrementJotai");
+    setJCount(jCount + 1);
+  }, [jCount, setJCount]);
+
   return (
-    <div>
+    <div style={{ display: "flex", flexDirection: "column", gap: "10px" }}>
       <div>Recoil Count: {rCount}</div>
       <div>Jotai Count: {jCount}</div>
-      <button onClick={increment}>increment</button>
+      <button onClick={incrementRecoil}>
+        increment recoil count state directly
+      </button>
+      <button onClick={incrementJotai}>
+        increment count via jotai to recoil proxy
+      </button>
     </div>
   );
 };
 
 export const JotaiRecoilBridge = () => {
   return (
-    <Provider store={jotaiStore}>
+    <Provider>
       <RecoilRoot>
-        <Count />
+        <RecoilStoreProvider>
+          <Count />
+        </RecoilStoreProvider>
       </RecoilRoot>
     </Provider>
   );
